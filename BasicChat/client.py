aa3c50482d56b7c89a7ca98cd7f0d5be09457b10
Hyuni89 <cuhcuh1@naver.com>
2018-03-22 17:52:58 +0900

    update

--- BasicChat/client.py
+++ BasicChat/client.py
@@ -2,6 +2,15 @@
 
 import socket
 import sys
+import threading
+
+def listen(client):
+    while True:
+        data = client.recv(1024).strip()
+        print(data.decode("utf-8"))
+
+        if not data:
+            break
 
 HOST = "localhost"
 PORT = 8888
@@ -11,15 +20,25 @@ if len(sys.argv) > 1:
     print("proc arg")
     pass
 
+print("Your ID : ", end="")
+ID = input()
+if len(ID) > 0xFF:
+    ID = ID[:0xFF]
+
 client = socket.socket()
 res = client.connect(ADDR)
-print(res)
+
+t = threading.Thread(target=listen, args=(client,))
+t.daemon = True
+t.start()
+
+client.send(ID.encode("utf-8"))
 
 while True:
     try:
         data = sys.stdin.readline()
-        res = client.send(data.encode("utf-8"))
-        print(res)
+        data = data.encode("utf-8")
+        res = client.send(data)
     except KeyboardInterrupt:
         break
 
--- BasicChat/server.py
+++ BasicChat/server.py
@@ -5,16 +5,52 @@ import socket
 import sys
 
 def handleUsers(client):
+    global userCount, clientList
+
+    ID = client.recv(1024).strip()
+    ID = ID.decode("utf-8")
+    addingClient(ID, client, True)
+    broadCast(ID, "has been joined!!!")
+
     while True:
-        data = client.recv(1024).decode("utf-8").strip()
+        data = client.recv(1024).strip()
         if not data:
             break
-        print(data)
-    
+        data = data.decode("utf-8")
+        print(ID, data)
+        broadCast(ID, data)
+
+    addingClient(ID, client, False)
+    broadCast(ID, "has been leaved!!!")
+
+def addingClient(ID, client, flag):
+    global lock, userCount, clientList
+
+    lock.acquire()
+    if flag is True:
+        if ID not in clientList:
+            userCount += 1
+            clientList[ID] = client
+    else:
+        if ID in clientList:
+            userCount -= 1
+            del clientList[ID]
+    lock.release()
+
+def broadCast(ID, string):
+    global clientList
+    data = "[" + ID + "] " + string
+    data = data.encode("utf-8")
+
+    for client in clientList.values():
+        res = client.send(data)
 
 HOST = "localhost"
 PORT = 8888
 ADDR = (HOST, PORT)
+lock = threading.Lock()
+userCount = 0
+clientList = dict()
 
 if len(sys.argv) > 1:
     print("proc arg")
@@ -29,10 +65,17 @@ while True:
         client, address = server.accept()
         print(client, address)
     except KeyboardInterrupt:
+        if len(clientList) > 0:
+            msg = "Server Closed... Sorry!"
+            broadCast("", msg.encode("utf-8"))
+
+            for c in clientList.values():
+                c.close()
         break
 
     if client is not None:
         t = threading.Thread(target=handleUsers, args=(client,))
+        t.daemon = True
         t.start()
 
 server.close()
